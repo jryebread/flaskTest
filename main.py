3aa16fe6f197ba636a1e5b73dce6397ef3aa8ba2
jryebread <jriback144@gmail.com>
2020-04-04 20:04:32 -0700

    new push

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from flask import request
+import sys
+from sys import stderr
+
+def print_err(*args, **kwargs):
+    print(*args, file=stderr, **kwargs)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+    ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
+    print('USER IP IS: ', ip, file=sys.stderr)
+    print_err("IP IS RECORDED AS: ", ip)
     return render_template("template.html")
 
 
@@ -16,3 +25,7 @@ def about():
 if __name__ == "__main__":
 
     app.run(debug=True)
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        print(request.environ['REMOTE_ADDR'])
+    else:
+        print(request.environ['HTTP_X_FORWARDED_FOR'])  # if behind a proxy
